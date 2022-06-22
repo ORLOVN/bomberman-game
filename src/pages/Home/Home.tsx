ff91c7725b2ff6800498ba3e0ca5e24de08651d3
Ildar <37474066+bagamanshin@users.noreply.github.com>
2022-06-22 18:09:02 +0700

    Add home page (#27)

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,10 +1,16 @@
 import { Flex } from "@chakra-ui/react";
 import React from "react";
 
+import styles from './Home.module.scss';
+
 export default function Home() {
   return (
-    <Flex align="center" direction="column" px="6" py="5">
-      <div>Home</div>
-    </Flex>
+    <Flex
+      align="center"
+      direction="column"
+      px="6"
+      py="5"
+      className={styles.wallpaper}
+    />
   );
 }
